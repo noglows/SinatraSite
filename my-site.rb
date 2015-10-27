f12a79c6651fd4a9ceee775edc8f8fcf3f3c03fb
noglows <jessica.noglows@outlook.com>
2015-10-27 14:03:44 -0700

    Added other HTML page files and updated my-site.rb file

--- my-site.rb
+++ my-site.rb
@@ -5,4 +5,20 @@ class MySite < Sinatra::Base
   get "/" do
     erb :index
   end
+
+  get "/about.html" do
+    erb :about
+  end
+
+  get "/blog.html" do
+    erb :blog
+  end
+
+  get "/projects.html" do
+    erb :projects
+  end
+
+  get "/index.html" do
+    erb :index
+  end
 end
