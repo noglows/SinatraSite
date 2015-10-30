dd52cd1356d0b98664550168b13bf50d46325595
noglows <jessica.noglows@outlook.com>
2015-10-30 09:49:55 -0700

    Added calculations for age for self and all pets/plants

--- my-site.rb
+++ my-site.rb
@@ -26,6 +26,32 @@ class MySite < Sinatra::Base
     return @selected_gif
   end
 
+
+  def calculate_age(year,month,day)
+    birthday = Time.new(year,month,day)
+    now = Time.now.utc
+    age = now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+    return age
+  end
+
+  def age_hash
+    all_ages = {}
+    all_ages["Jessica"] = calculate_age(1991,3,21)
+    all_ages["Kepler"] = calculate_age(2013,2,21)
+    all_ages["Jekyll"] = calculate_age(2014,11,30)
+    all_ages["Hyde"] = calculate_age(2014,11,30)
+    all_ages["Sil"] = calculate_age(2015,6,30)
+    all_ages["JimJohn"] = calculate_age(2015,7,21)
+    all_ages["Lemondrop"] = calculate_age(2015,7,21)
+
+    all_ages.each do |key, value|
+      if value < 1
+        all_ages[key] = "<1"
+      end
+    end
+    return @all_ages = all_ages
+  end
+
   get "/" do
     @title = "Home"
     @page_header = "Jessica Noglows"
@@ -44,6 +70,7 @@ class MySite < Sinatra::Base
     @title = "About Me"
     @page_header = "About Me"
     set_variables
+    age_hash
     erb :about
   end
 
